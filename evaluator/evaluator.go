967ae481bd87133e18723e5761e1ba5a5034d996
jhonnyV-V <jhonny_varela_visbal@hotmail.com>
2024-05-12 16:06:15 -0400

    adding evaluator

---
+++ evaluator/evaluator.go
@@ -0,0 +1,16 @@
+package evaluator
+
+import (
+	"mokey-type/ast"
+	"mokey-type/object"
+)
+
+func Eval(node ast.Node) object.Object {
+	switch node := node.(type) {
+	case *ast.IntegerLiteral:
+		return &object.Integer{Value: node.Value}
+	case *ast.Boolean:
+		return &object.Boolean{Value: node.Value}
+	}
+	return nil
+}
